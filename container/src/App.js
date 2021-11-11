ab1c574cb6e568286f7cd3cfea7f69cca84c5264
faizansayyed <faizansayyed36@gmail.com>
2021-11-11 10:39:00 +0530

    test container workflow

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ export default () => {
   return (
     <div>
       <h1>Hi there!</h1>
-      <div>Container</div>
+      <div>Container testing</div>
       <hr />
       <MarketingApp />
     </div>
